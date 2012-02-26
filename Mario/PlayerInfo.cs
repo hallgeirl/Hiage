b7477b98a8e88cf4ebd589b8d83c28ee676bf96f
Hallgeir Lien <hallgeir@hallgeir-desktop.(none)>
2012-02-26 14:08:20 +0100

    cleanup

---
+++ Mario/PlayerInfo.cs
@@ -0,0 +1,21 @@
+
+using System;
+
+namespace Mario
+{
+	/// <summary>
+	/// Contains info about the player
+	/// </summary>
+	public class PlayerInfo
+	{
+		public PlayerInfo ()
+		{
+		}
+		
+		public int Lives
+		{
+			get;
+			set;
+		}
+	}
+}

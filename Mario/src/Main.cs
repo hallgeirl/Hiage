ea52d2d63198437eff00221811a3d48366dfb26a
Hallgeir Lien <hallgeir@hallgeir-N61Ja.(none)>
2012-05-01 21:45:58 +0200

    fixed start level for mario

--- Mario/src/Main.cs
+++ Mario/src/Main.cs
@@ -15,7 +15,7 @@ namespace Mario
 			//Log.OutputStreamWriter = new StreamWriter("log.txt");
 
 			PlayerState initialState = new PlayerState();
-			game.PushState(new LevelState(initialState, game, "pipeland"));
+			game.PushState(new LevelState(initialState, game, "level1"));
 			//game.PushState(new LevelState(null, game, "minimap"));
 			//game.PushState(new LevelState(null, game, "test_multi"));
 			
